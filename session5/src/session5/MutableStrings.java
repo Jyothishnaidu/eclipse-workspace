ec659a9b84e408ab969d04dc7788827cb619d4f0
unknown <mjyothish1437@gmail.com>
2022-02-18 19:08:43 +0530

    session5

---
+++ session5/src/session5/MutableStrings.java
@@ -0,0 +1,31 @@
+package session5;
+
+public class MutableStrings {
+public static void main(String[] args) {
+		
+		String name = new String("John");
+		String fullName = name + " Watson"; //name.concat(" Watson");
+		
+		System.out.println("name is "+name); // John
+		System.out.println("fullName is: "+fullName); // John Watson
+		
+
+		// Mutable Version of String in Java
+		// If we wish to perform some manipulation on Strings, -> USE StringBuffer :)
+		// StringBuffer is thread-safe
+		StringBuffer buffer = new StringBuffer("John");
+		buffer.append(" Watson");
+		System.out.println("buffer is: "+buffer);
+		
+		// StringBuffer cannot be created in an interned way
+		// It will always be created as an Object
+		//StringBuffer b1 = "Hello"; // error
+		
+		// Mutable Version of String in Java 
+		// A bit faster as compared to StringBuffer
+		// StringBuilder is not thread-safe
+		StringBuilder builder = new StringBuilder("Fionna");
+		builder.append(" Flynn");
+		System.out.println("builder is: "+builder);
+     }
+}
---
+++ session5/src/session5/StringBuiltInMethods.java
@@ -0,0 +1,58 @@
+package session5;
+
+public class StringBuiltInMethods {
+public static void main(String[] args) {
+		
+		String names = "john, jennie, jim, jack, joe";
+		
+		int length = names.length();
+		System.out.println("Length of names is: "+length);
+		
+		int idx = names.indexOf('h');
+		System.out.println("idx of h is: "+idx);
+		
+		idx = names.lastIndexOf('o');
+		System.out.println("idx of o from the end is: "+idx);
+		
+		idx = names.indexOf("jim"); // it will give the index from where, jim's j starts :)
+		System.out.println("jim index is: "+idx); // 14
+		
+		//String name = names.substring(idx); // 14 | from index 14 to end
+		String name = names.substring(14, 17); // 14, 15, 16
+		System.out.println(name);
+		
+		//char ch = names.charAt(idx);
+		char ch = names.charAt(length-1);
+		System.out.println("ch is: "+ch); // e
+		
+		char[] chArr = names.toCharArray();
+		int count = 0;
+		for(char chr : chArr) {
+			System.out.print(chr+" ");
+			if(chr == 'j') {
+				count++;
+			}
+		}
+		System.out.println();
+		System.out.println("j appears "+count+" times");
+		
+		String[] strArr = names.split(",");
+		for(String str : strArr) {
+			System.out.println(str.trim()); // any extra spaces from the front and end will be removed
+		}
+		
+		String email = "john@example.com";
+		if(email.contains("@") && email.contains(".com")) {
+			System.out.println("A Valid Email");
+		}else {
+			System.out.println("Invalid Email");
+		}
+			
+		String songName = "hello.mp3";
+		if(songName.endsWith(".mp3")) { // startsWith
+			System.out.println("We can play this audio file");
+		}else {
+			System.out.println("invalid audio file");
+		}
+     }
+}
